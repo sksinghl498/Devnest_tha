95254ad67adf80b73a7ca1c6e54911f61997e5ef
Shivam <shivamsinghl498@gmail.com>
2021-07-03 02:19:11 +0530

    Day15tha

---
+++ day15tha/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <>
+  <h1>Hello world</h1>
+  <h1>Shivam kumar singh</h1>
+  </>,
+  document.getElementById('root')
+);
