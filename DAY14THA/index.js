4480dfbb85d3411ac8624f8417fbaab5d32ca1da
Shivam <shivamsinghl498@gmail.com>
2021-07-05 00:02:52 +0530

    day14tha

--- DAY14THA/index.js
+++ DAY14THA/index.js
@@ -1,5 +1,14 @@
 
 
 function myfunction(){
-    return document.getElementById('s1').innerHTML= "Hello! how r u?"
+    return document.getElementById('s1').innerHTML= "Hello! how r u?";
+}
+
+function myfunction2(){
+    return document.getElementById('s2').innerText="This is double click";
+}
+
+function myfunction3(){
+    var x= document.getElementById("s3");
+    x.value= x.value.toUpperCase();
 }
