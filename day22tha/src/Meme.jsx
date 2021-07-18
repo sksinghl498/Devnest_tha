9ade17daba1c19de528b5373e02bcf2195f5a256
Shivam <shivamsinghl498@gmail.com>
2021-07-18 18:42:01 +0530

    day22tha

--- day22tha/src/Meme.jsx
+++ day22tha/src/Meme.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
+
 const Meme = ({meme,setMeme})=>{
+    const [form,setForm]=useState({
+        template_id: meme.id,
+        username: "sksinghl498",
+        password: "sksinghl498",
+        boxes: [],
+    });
+    const generateMeme = ()=>{
+        console.log(form);
+    };
     return (
         <div className="meme">
             <img src={meme.url} alt =""/>
             <div>
                 {
                     [...Array(meme.box_count)].map((_,index)=>(
-                        <input type="text" placeholder={`Meme caption ${index}`}/>
+                        <input 
+                        key ={index}
+                        type="text" placeholder={`Meme caption ${index}`} 
+                            onChange= {(e)=>{
+                                const newBoxes = form.boxes;
+                                newBoxes[index]= {text: e.target.value};
+                                setForm({...form, boxes: newBoxes})
+                            }}
+                        />
                     ))
                 }
             </div>
             <div>
-                <button>Generate Meme</button>
+                <button onClick={generateMeme}>Generate Meme</button>
                 <button onClick={()=>{
                     setMeme(null);
                 }}>Choose Template</button>
