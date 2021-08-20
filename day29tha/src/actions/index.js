336130fb73b645bb8006ab12a4bc69f992956677
Shivam <shivamsinghl498@gmail.com>
2021-08-20 16:25:57 +0530

    day29tha Weather api with Redux

---
+++ day29tha/src/actions/index.js
@@ -0,0 +1,21 @@
+const updatePlace =(place)=>{
+    return {
+        type: "UPDATE_PLACE",
+        payload: place,
+    };
+};
+
+const updatePlaceData=(place)=>{
+    return(dispatch)=>{
+    fetch(`https://api.weatherapi.com/v1/current.json?key=06c51334044f4ddba98191926211808&q=${place}`)
+    .then((res)=> res.json())
+    .then((data)=>{
+    dispatch({
+        type: "UPDATE_PLACE_DATA",
+        payload: data,
+       });
+    }); 
+    };
+};
+
+export {updatePlace,updatePlaceData};
