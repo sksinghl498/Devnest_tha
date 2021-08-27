b5eec77c257d4e33d019a12d9ba1f1b3867031a6
Shivam <shivamsinghl498@gmail.com>
2021-08-27 23:46:49 +0530

    E-commerce

---
+++ day30-31-tha/src/redux/reducers/productReducer.js
@@ -0,0 +1,16 @@
+const intialState= {
+    products:[{
+        id: 1,
+        title: "shivam",
+        category: "coder",
+    },],
+};
+export const productReducer=(state,{type,payload})=>{
+    switch(action.type){
+        case SET_PRODUCTS:
+            return state;
+
+        default:
+            return state;
+    }
+}
