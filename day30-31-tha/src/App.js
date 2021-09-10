1163e416a0d77203111d922fcb24648b0251d99c
Shivam <shivamsinghl498@gmail.com>
2021-09-10 14:06:36 +0530

    E-commerce Website with react-redux

--- day30-31-tha/src/App.js
+++ day30-31-tha/src/App.js
@@ -1,20 +1,22 @@
-import Header from './containers/Header';
-import ProductListing from './containers/ProductListing';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
-import ProductDetail from './containers/ProductDetail';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
+import "./App.css";
+import ProductDetails from "./containers/ProductDetails";
+
 function App() {
   return (
-    <>
-    <Router>
-    <Header/>
-    <Switch>
-    <Route path="/" exact component={ProductListing}/>
-    <Route path="/product/:productId" exact component={ProductDetail}/>
-    <Route>404 Not found</Route>
-    <ProductListing/>
-    </Switch>
-    </Router>
-    </>
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
--- day30-31-tha/src/containers/ProductListing.js
+++ day30-31-tha/src/containers/ProductListing.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import { useSelector,useDispatch } from 'react-redux';
-import ProductComponent from './ProductComponent';
-import axios from 'axios';
-import { useEffect } from 'react';
-import { setProducts } from '../redux/actions/productActions';
-const ProductListing=()=>{
-    const products= useSelector((state)=> state);
-    const dispatch = useDispatch();
-    const fetchProducts= async ()=>{
-        const response= await axios.get("https://fakestoreapi.com/products").catch((err)=> {
-            console.log("err",err);
-        });
+import React, { useEffect, useCallback, useMemo } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../redux/actions/productsActions";
+import ProductComponent from "./ProductComponent";
 
-        dispatch(setProducts(response.data));
-    };
-    console.log("products: ",products);
+const ProductPage = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const dispatch = useDispatch();
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Err: ", err);
+      });
+    dispatch(setProducts(response.data));
+  };
 
-    useEffect(()=>{
-        fetchProducts();
-    },[]);
-    return (
-        <div className="ui grid container">
-            <ProductComponent/>
-        </div>
-    );
-}
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-export default ProductListing;
+  console.log("Products :", products);
+  return (
+    <div className="ui grid container">
+      <ProductComponent />
+    </div>
+  );
+};
+
+export default ProductPage;
