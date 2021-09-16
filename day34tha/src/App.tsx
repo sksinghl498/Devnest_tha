e5bff4ddd663fc0d10647d702d345c8f9f22bc78
Shivam <shivamsinghl498@gmail.com>
2021-09-16 19:03:38 +0530

    instagram Clone

--- day34tha/src/App.tsx
+++ day34tha/src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useSelector,useDispatch } from 'react-redux';
+import { addNote } from './actions';
 import { NewNoteInput } from './NewNoteInput';
-
+import {NotesState} from './notesReducer';
 function App() {
+  const notes= useSelector<NotesState,NotesState["notes"]>((state)=> state.notes)
+
+  const dispatch= useDispatch()
+
+
+  const onAddNote= (note: string)=>{
+    dispatch(addNote(note))
+  };
   return (
     <>
-    <NewNoteInput/>
+    <NewNoteInput addNote={onAddNote} />
     <hr/>
     <ul>
-      <li>Some note</li>
+      {notes.map((note)=>{
+      return  <li key={note}>{note}</li>;
+      })}
     </ul>
     </>
   );
--- day34tha/src/NewNoteInput.tsx
+++ day34tha/src/NewNoteInput.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
 interface NewNoteInputProps{
     addNote(note: string): void;
 }
-export const NewNoteInput=()=>{
+
+export const NewNoteInput: React.FC<NewNoteInputProps> =({addNote})=>{
+    const [note,setNote] = React.useState("")
+
+    const updateNote =(event: ChangeEvent<HTMLInputElement>)=>{
+        setNote(event.target.value)
+    }
+
+    const onAddNoteClick=()=>{
+        addNote(note);
+        setNote("")
+    }
+
     return(
         <div>
-      <input type="text" name="note" placeholder="Note"/>
-      <button>Add note</button>
+      <input onChange={updateNote} type="text" name="note" placeholder="Note"/>
+      <button onClick={onAddNoteClick}>Add note</button>
      </div>
     );
 }
